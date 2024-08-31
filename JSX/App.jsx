c25b64b56cba8a323deef16f212ac3d6c74d3fee
b0b-e <ayushbanerjeestudent@gmail.com>
2024-08-31 22:25:45 +0530

    Added hamburger menu

--- JSX/App.jsx
+++ JSX/App.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 import Navbar from './Navbar/Navbar.jsx'
 import Body from './Body/Body.jsx'
 import Footer from './Footer/Footer.jsx'
-// import './App.css'
-import '../CSS/App.css'
+import './../CSS/App.css'
 function App() {
   return (
     <div className='app'>
--- JSX/Body/NearYou.jsx
+++ JSX/Body/NearYou.jsx
@@ -1,27 +1,188 @@
 import React from "react";
-// import "../Styles/NearYou.css"
-import "../../CSS/Styles/NearYou.css";
-let obj1 = { arriving: false, time: "1:30PM", no: "G7X", from: "Demo Location", to: "Demo Location2", ac: true, ev: true };
-let obj2 = { arriving: "true", time: "1:30PM", no: "B3K", from: "Demo Location", to: "Demo Location2", ac: true, ev: true };
-let obj3 = { arriving: "true", time: "1:30PM", no: "Q8J", from: "Demo Location", to: "Demo Location2", ac: true, ev: true };
-let obj4 = { arriving: "true", time: "1:30PM", no: "Z1W", from: "Demo Location", to: "Demo Location2", ac: true, ev: true };
-let obj5 = { arriving: "true", time: "1:30PM", no: "R5N", from: "Demo Location", to: "Demo Location2", ac: true, ev: true };
-let obj6 = { arriving: false, time: "1:30PM", no: "T2L", from: "Demo Location", to: "Demo Location2", ac: true, ev: true };
-let obj7 = { arriving: "true", time: "1:30PM", no: "F8P", from: "Demo Location", to: "Demo Location2", ac: true, ev: true };
-let obj8 = { arriving: "true", time: "1:30PM", no: "N4R", from: "Demo Location", to: "Demo Location2", ac: true, ev: true };
-let obj9 = { arriving: "true", time: "1:30PM", no: "C6V", from: "Demo Location", to: "Demo Location2", ac: true, ev: true };
-let obj10 = { arriving: "true", time: "1:30PM", no: "X9D", from: "Demo Location", to: "Demo Location2", ac: true, ev: true };
-let obj11 = { arriving: false, time: "1:30PM", no: "H3K", from: "Demo Location", to: "Demo Location2", ac: true, ev: true };
-let obj12 = { arriving: "true", time: "1:30PM", no: "J7B", from: "Demo Location", to: "Demo Location2", ac: true, ev: true };
-let obj13 = { arriving: "true", time: "1:30PM", no: "V1M", from: "Demo Location", to: "Demo Location2", ac: true, ev: true };
-let obj14 = { arriving: "true", time: "1:30PM", no: "S4G", from: "Demo Location", to: "Demo Location2", ac: true, ev: true };
-let obj15 = { arriving: "true", time: "1:30PM", no: "W8Q", from: "Demo Location", to: "Demo Location2", ac: true, ev: true };
-let obj16 = { arriving: false, time: "1:30PM", no: "D9A", from: "Demo Location", to: "Demo Location2", ac: true, ev: true };
-let obj17 = { arriving: "true", time: "1:30PM", no: "K5L", from: "Demo Location", to: "Demo Location2", ac: true, ev: true };
-let obj18 = { arriving: "true", time: "1:30PM", no: "M2T", from: "Demo Location", to: "Demo Location2", ac: true, ev: true };
-let obj19 = { arriving: "true", time: "1:30PM", no: "R6Z", from: "Demo Location", to: "Demo Location2", ac: true, ev: true };
-let obj20 = { arriving: "true", time: "1:30PM", no: "L7E", from: "Demo Location", to: "Demo Location2", ac: true, ev: true };
-
+import "../../CSS/Styles/NearYou.css"
+let obj1, obj2, obj3, obj4, obj10, obj9, obj8, obj7, obj6, obj5, obj11, obj12, obj13, obj14, obj15, obj16, obj17, obj18, obj19, obj20;
+{
+obj1 = {
+    arriving: false,
+    time: "1:30PM",
+    no: "G7X",  // Randomized value
+    from: "Demo Location",
+    to: "Demo Location",
+    ac: true,
+    ev: true
+}
+obj2 = {
+    arriving: "true",
+    time: "1:30PM",
+    no: "B3K",  // Randomized value
+    from: "Demo Location",
+    to: "Demo Location",
+    ac: true,
+    ev: true
+}
+obj3 = {
+    arriving: "true",
+    time: "1:30PM",
+    no: "Q8J",  // Randomized value
+    from: "Demo Location",
+    to: "Demo Location",
+    ac: true,
+    ev: true
+}
+obj4 = {
+    arriving: "true",
+    time: "1:30PM",
+    no: "Z1W",  // Randomized value
+    from: "Demo Location",
+    to: "Demo Location",
+    ac: true,
+    ev: true
+}
+obj5 = {
+    arriving: "true",
+    time: "1:30PM",
+    no: "R5N",  // Randomized value
+    from: "Demo Location",
+    to: "Demo Location",
+    ac: true,
+    ev: true
+}
+obj6 = {
+    arriving: false,
+    time: "1:30PM",
+    no: "T2L",  // Randomized value
+    from: "Demo Location",
+    to: "Demo Location",
+    ac: true,
+    ev: true
+}
+obj7 = {
+    arriving: "true",
+    time: "1:30PM",
+    no: "F8P",  // Randomized value
+    from: "Demo Location",
+    to: "Demo Location",
+    ac: true,
+    ev: true
+}
+obj8 = {
+    arriving: "true",
+    time: "1:30PM",
+    no: "N4R",  // Randomized value
+    from: "Demo Location",
+    to: "Demo Location",
+    ac: true,
+    ev: true
+}
+obj9 = {
+    arriving: "true",
+    time: "1:30PM",
+    no: "C6V",  // Randomized value
+    from: "Demo Location",
+    to: "Demo Location",
+    ac: true,
+    ev: true
+}
+obj10 = {
+    arriving: "true",
+    time: "1:30PM",
+    no: "X9D",  // Randomized value
+    from: "Demo Location",
+    to: "Demo Location",
+    ac: true,
+    ev: true
+}
+obj11 = {
+    arriving: false,
+    time: "1:30PM",
+    no: "H3K",  // Randomized value
+    from: "Demo Location",
+    to: "Demo Location",
+    ac: true,
+    ev: true
+}
+obj12 = {
+    arriving: "true",
+    time: "1:30PM",
+    no: "J7B",  // Randomized value
+    from: "Demo Location",
+    to: "Demo Location",
+    ac: true,
+    ev: true
+}
+obj13 = {
+    arriving: "true",
+    time: "1:30PM",
+    no: "V1M",  // Randomized value
+    from: "Demo Location",
+    to: "Demo Location",
+    ac: true,
+    ev: true
+}
+obj14 = {
+    arriving: "true",
+    time: "1:30PM",
+    no: "S4G",  // Randomized value
+    from: "Demo Location",
+    to: "Demo Location",
+    ac: true,
+    ev: true
+}
+obj15 = {
+    arriving: "true",
+    time: "1:30PM",
+    no: "W8Q",  // Randomized value
+    from: "Demo Location",
+    to: "Demo Location",
+    ac: true,
+    ev: true
+}
+obj16 = {
+    arriving: false,
+    time: "1:30PM",
+    no: "D9A",  // Randomized value
+    from: "Demo Location",
+    to: "Demo Location",
+    ac: true,
+    ev: true
+}
+obj17 = {
+    arriving: "true",
+    time: "1:30PM",
+    no: "K5L",  // Randomized value
+    from: "Demo Location",
+    to: "Demo Location",
+    ac: true,
+    ev: true
+}
+obj18 = {
+    arriving: "true",
+    time: "1:30PM",
+    no: "M2T",  // Randomized value
+    from: "Demo Location",
+    to: "Demo Location",
+    ac: true,
+    ev: true
+}
+obj19 = {
+    arriving: "true",
+    time: "1:30PM",
+    no: "R6Z",  // Randomized value
+    from: "Demo Location",
+    to: "Demo Location",
+    ac: true,
+    ev: true
+}
+obj20 = {
+    arriving: "true",
+    time: "1:30PM",
+    no: "L7E",  // Randomized value
+    from: "Demo Location",
+    to: "Demo Location",
+    ac: true,
+    ev: true
+}
+}
 let data = [obj1, obj2, obj3, obj4, obj10, obj9, obj8, obj7, obj6, obj5, obj11, obj12, obj13, obj14, obj15, obj16, obj17, obj18, obj19, obj20];
 export default function NearYou(props) {
     let buses = data.map((x) => {
@@ -35,9 +196,11 @@ export default function NearYou(props) {
                         </div>
                         <div className="bus-left-line2">
                             <div className="bus-from">
-                                From: <b>{`${x.from}`}</b>
+                                From: <b>Demo Location</b>
                             </div>
+                            <div className='arrow-img-container'>
                             <img className="bus-arrow" src="/images/bus-arrow.png" />
+                            </div>
                             <div className="bus-to">
                                 To: <b>{`${x.to}`}</b>
                             </div>
